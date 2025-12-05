bebbf4f1d0c3abd383db733967170bd9dd2d1771
Manouk <0354082@student.rocvantwente.nl>
2025-12-05 11:22:31 +0100

    Add global styles, Navbar, Footer, and Homepage components
    - Created global CSS file to set background image for the body.
    - Implemented Navbar component with basic structure.
    - Implemented Footer component with basic structure.
    - Developed Homepage component that includes a container with a greeting message.
    - Updated package-lock.json for project dependencies.

---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../css/global.css'
+
+function Homepage() {
+
+    return (
+        <>
+        <div className='container'>
+<p> hai</p>
+        </div>
+        </>
+    )
+}
+
+export default Homepage
